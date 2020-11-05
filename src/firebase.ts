fadfcdb7c13bab82e937020f6a680de8aea44e84
arindamaluni <aaluni@gmail.com>
2020-11-05 18:52:23 +0000

    config : firebase app changes

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,16 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-// Replace the following with the config for your own Firebase project
-// https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyAqk1HRPmushNirOZ0LdGcUQVocd7LWyAE",
-  authDomain: "daily-moments-d677f.firebaseapp.com",
-  databaseURL: "https://daily-moments-d677f.firebaseio.com",
-  projectId: "daily-moments-d677f",
-  storageBucket: "daily-moments-d677f.appspot.com",
-  messagingSenderId: "309263458225",
-  appId: "1:309263458225:web:0753a835b14f4828366790"
+  apiKey: "AIzaSyDdklRLWgGr1hvQTAHgDeconA4D19ZWftM",
+  authDomain: "around-a86ff.firebaseapp.com",
+  databaseURL: "https://around-a86ff.firebaseio.com",
+  projectId: "around-a86ff",
+  storageBucket: "around-a86ff.appspot.com",
+  messagingSenderId: "958820447540",
+  appId: "1:958820447540:web:0b601e35e8b1a405642161"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
