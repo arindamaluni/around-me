cadb0f38e3bc7c23f565327af1c2b7c1c72a3138
arindamaluni <aaluni@gmail.com>
2020-11-06 20:04:11 +0000

    fix : eventlist swipe - conjoin seperation

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -12,32 +12,35 @@ function EventList({eventList}) {
   eventList = eventList.length? eventList :list;
 
   return (
-    <IonItemSliding>
+    <>
       {eventList.map(event => {
         // return <EventItem key={event._id} event={event}/>
-        return (<IonItem href={`${ROUTE_EVENT_PATH}${event.id}`} key={event.id}>
-        <IonLabel>
-          <h2>{event.title}</h2>
-          <p>{moment.utc(event.date).format('LLLL')}</p>
-          <p>{event.venue}</p>
-        </IonLabel>
-        <IonNote slot="end">
-          <p>{moment.utc(event.date).format('LT')}</p>
-        </IonNote>
-        <IonItemOptions>
-          <IonItemOption color="primary">
-            <IonIcon slot="end" ios={ellipsisHorizontal} md={ellipsisVertical}></IonIcon>
-            More
-          </IonItemOption>
-          <IonItemOption color="secondary">
-            <IonIcon slot="end" icon={archive} />
-            Archive
-          </IonItemOption>
-        </IonItemOptions>
-      </IonItem>)
-
+        return ( 
+      <IonItemSliding>
+        <IonItem href={`${ROUTE_EVENT_PATH}${event.id}`} key={event.id}>
+          <IonLabel>
+            <h2>{event.title}</h2>
+            <p>{moment.utc(event.date).format('LLLL')}</p>
+            <p>{event.venue}</p>
+          </IonLabel>
+          <IonNote slot="end">
+            <p>{moment.utc(event.date).format('LT')}</p>
+          </IonNote>
+          <IonItemOptions>
+            <IonItemOption color="primary">
+              <IonIcon slot="end" ios={ellipsisHorizontal} md={ellipsisVertical}></IonIcon>
+              More
+            </IonItemOption>
+            <IonItemOption color="secondary">
+              <IonIcon slot="end" icon={archive} />
+              Archive
+            </IonItemOption>
+          </IonItemOptions>
+        </IonItem>
+      </IonItemSliding>
+      )
       })}
-    </IonItemSliding>
+    </>
     );
   }
 export default EventList;
