cde66e0ade6b7bc0a887ff2c8198406171bc2ac6
arindamaluni <aaluni@gmail.com>
2020-11-06 15:51:13 +0000

    refactor : scss introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
 import { IonApp, IonLoading, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-import '@ionic/react/css/display.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/float-elements.css';
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/typography.css';
 /* Application Imports */
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -30,6 +17,7 @@ import NewEvent from './pages/NewEvent';
 import RegisterPage from './pages/RegisterPage';
 import * as constants from './route-constants';
 import store from './store';
+
 
 
 
